110d68c6b23a9512537f4b3e9f6da2495047eb20
Parag Amrutkar <paragamrutkar1103@gmail.com>
2025-11-29 23:29:32 -0500

    Replace PNG background with SVG; update homepage layout accordingly

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "@/components/SearchBar"
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-[url('/background_homepage.png')] bg-cover bg-center pb-24">
+    <div className="relative min-h-screen bg-[url('/background_homepage.svg')] bg-cover bg-center pb-24">
       {/* Page content goes here */}
 
       <div className="fixed bottom-10 left-1/2 w-full max-w-md -translate-x-1/2 px-4">
